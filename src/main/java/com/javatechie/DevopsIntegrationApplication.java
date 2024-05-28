55e91a53c225b9e17a0a66cb55d73bba023ec6ee
munajatadnan <munajat.adnan@gmail.com>
2024-05-28 21:54:38 +0800

    fix some addition issue in java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -3,25 +3,26 @@ package com.javatechie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DevopsIntegrationApplication {
 
-	@GetMapping
-	public String message(){
-		return "welcome to devops automation";
-	}
+    @GetMapping("/message")
+    public String message() {
+        return "welcome to devops automation";
+    }
 
-	@GetMapping
-	public String add(x, y){
-		System.out.println("test");
-		return x + y;
-	}
+    @GetMapping("/add")
+    public String add(@RequestParam int x, @RequestParam int y) {
+        System.out.println("test");
+        return String.valueOf(x + y);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsIntegrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsIntegrationApplication.class, args);
+    }
 
 }

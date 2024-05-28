ee4d7111accedc34fa72322da03d95b816cfb143
afiqasyraf9712 <afiqasyraf9712@gmail.com>
2024-05-28 19:11:25 +0800

    feat(message): more text update

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to devops";
+		return "welcome to devops automation";
 	}
 
 	public static void main(String[] args) {
